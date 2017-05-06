8196109f7cc63cb195c6d2fa7b875656a1f0c493
Paul Barker <paul_barker@live.com>
2017-05-06 05:48:01 -0700

    More practice with proccess.on()

--- index.js
+++ index.js
@@ -12,3 +12,43 @@ if ( ! user || ! greeting ) {
 } else {
 	console.log(`Welcome ${user}, ${greeting}`);
 }
+
+const waitTime = 3000;
+let answers = [];
+
+const questions = [
+	"What is your name?",
+	"What is your fav hobby?",
+	"What is your preffered programming language?"
+];
+
+function ask(i) {
+	'use strict';
+	process.stdout.write(`\n${questions[i]}`);
+	process.stdout.write(" > ");
+}
+
+process.stdin.on('data', (data) => {
+	answers.push(data.toString().trim());
+	if ( answers.length < questions.length ) {
+		ask(answers.length);
+	} else {
+		console.log("Waiiiiit for it...");
+		setTimeout(() => {
+			process.exit();
+		}, waitTime);
+
+	}
+} );
+
+process.on('exit', () => {
+	'use strict';
+	process.stdout.write('\n\n');
+	process.stdout.write(`${answers[1]} is for losers, ${answers[0]}. You need to write more ${answers[2]}.`);
+	process.stdout.write('\n\n');
+});
+
+ask(0);
+
+
+
