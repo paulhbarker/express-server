dae16711a3b408f0828069e36899fe367e67fb1f
Paul Barker <paul_barker@live.com>
2017-05-06 07:09:53 -0700

    Refactor Person to CommonJS module exports.

--- events.js
+++ events.js
@@ -1,17 +1,16 @@
-const EventEmitter = require('events').EventEmitter;
-const util = require('util');
-
-class Person extends EventEmitter {
-	constructor(name) {
-		super();
-		this.name = name;
-	}
-}
+const Person = require('./lib/Person');
 
 const abe = new Person('Abraham Lincoln');
+const george = new Person('George Washington');
+
+
+george.on('speak', function(said) {
+	console.log(`${this.name}: ${said}`);
+})
 
 abe.on('speak', function(said) {
 	console.log(`${this.name}: ${said}`);
 })
 
 abe.emit('speak', 'Four score and seven years ago...');
+george.emit('speak', 'It is far better to be alone, than to be in bad company.');
