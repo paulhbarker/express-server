9a2b7489be206a0b3e7829ad91ae66bbbd88f972
Paul Barker <paul_barker@live.com>
2017-05-08 01:55:37 -0700

    add file writing to io prompt

--- io/prompt.js
+++ io/prompt.js
@@ -1,6 +1,7 @@
 // Using readline to prompt for input
 
 const readline = require('readline');
+const fs = require('fs');
 const rl = readline.createInterface(process.stdin, process.stdout);
 const realPerson = {
 	name: '',
@@ -9,6 +10,10 @@ const realPerson = {
 
 rl.question('What is your grandmother\'s name? ', (answer) => {
 	realPerson.name = answer;
+	const outFile = './io/' + realPerson.name + ".md";
+
+	fs.writeFileSync(outFile, `${realPerson.name}\n=================\n\n`);
+
 	rl.setPrompt(`What would ${realPerson.name} say? `);
 	rl.prompt();
 	rl.on('line', (saying) => {
@@ -16,13 +21,16 @@ rl.question('What is your grandmother\'s name? ', (answer) => {
 			rl.close();
 		} else {
 			realPerson.sayings.push(saying.trim());
+
+			fs.appendFile(outFile, `* ${saying.trim()}\n`);
+
 			rl.setPrompt(`What would ${realPerson.name} say? ('exit' to leave) `);
 			rl.prompt();
 		}
-	})
+	});
 });
 
 rl.on('close', () => {
 	console.log("%s is a real person that says %j", realPerson.name, realPerson.sayings);
 	process.exit();
-})
+});
