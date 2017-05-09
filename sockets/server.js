26c560b4de14e2ccc88a906789a3dab710a2adb1
Paul Barker <paul_barker@live.com>
2017-05-08 22:03:32 -0700

    Sockets!

---
+++ sockets/server.js
@@ -0,0 +1,16 @@
+const WebSocketServer = require('ws').Server;
+const wss = new WebSocketServer({ port: 3000 });
+
+wss.on('connection', (ws) => {
+
+	ws.on('message', (message) => {
+		if (message === 'exit') {
+			ws.close();
+		} else {
+			wss.clients.forEach((client) => {
+				client.send(message);
+			});
+		}
+	});
+	ws.send('Welcome to this chat.');
+});
