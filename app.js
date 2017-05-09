a8d2435ed9945fa055cbdcfe123c6d0a8e5fe750
Paul Barker <paul_barker@live.com>
2017-05-08 21:48:12 -0700

    Our beautiful baby controller!

--- app.js
+++ app.js
@@ -1,14 +1,55 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
-// Middleware
-app.use((req, res, next) => {
-	console.log(`${req.method} request for '${req.url}'`);
-	next();
-})
+// Mutable list of terms. Our "API"
+let skierTerms = [
+	{
+		term: 'Rip',
+		defined: 'To move at a high rate of speed'
+	},
+	{
+		term: 'Huck',
+		defined: 'To throw your body off something, usually a natural feature like a cliff'
+	},
+	{
+		term: 'Chowder',
+		defined: 'Powder after it has been sufficiently skied'
+	}
+]
 
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+	console.log(`${req.method} request for '${req.url}' - ${JSON.stringify(req.body)}`);
+	next();
+});
+
+// Boot a static server serving files in this dir.
 app.use(express.static('./public'));
+
+// Allow cross-origin resource sharing.
+app.use(cors());
+
+
+// Routes
+app.get('/dictionary-api', (req, res) => {
+	res.json(skierTerms);
+});
+
+app.post('/dictionary-api', (req, res) => {
+	skierTerms.push(req.body)
+	res.json(skierTerms);
+});
+
+app.delete('/dictionary-api/:term', (req, res) => {
+	skierTerms = skierTerms.filter((def) => def.term.toLowerCase() !== req.params.term.toLowerCase());
+	res.json(skierTerms);
+});
+
 
 app.listen(3000);
 
